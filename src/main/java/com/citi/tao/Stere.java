ff6e219b8c32d3dde85b693df941701bbffd4e18
沈涛 <taoshen@ucdavis.edu>
2020-01-29 17:25:17 +0800

    st

--- src/main/java/com/citi/tao/Stere.java
+++ src/main/java/com/citi/tao/Stere.java
@@ -3,7 +3,7 @@ package com.citi.tao;
 public class Stere {
     public static void main(String[] args) {
         System.out.println("asddfbv");
-        System.out.println("abc");
+        System.out.println("abcd");
         System.out.println("imbrtrtyaiyu");
     }
 }
