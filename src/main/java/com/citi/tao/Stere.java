95261d4073223847cfa2b1e0d41e2579389612ee
ts15659 <st15316199026@163.com>
2020-01-29 10:38:45 +0800

    tt

--- src/main/java/com/citi/tao/Stere.java
+++ src/main/java/com/citi/tao/Stere.java
@@ -2,7 +2,7 @@ package com.citi.tao;
 
 public class Stere {
     public static void main(String[] args) {
-        System.out.println("asyouio");
+        System.out.println("abc");
         System.out.println("imbrtrtyaiyu");
     }
 }
